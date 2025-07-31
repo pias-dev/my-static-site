94c6846041a1595d8e996c3a26771c4a17a2b560
pias-dev <piaspaul12@gmail.com>
2025-07-31 22:38:32 +0600

    Delete assets directory

--- assets/dark-theme.js
+++
@@ -1,1 +0,0 @@
-if(localStorage.getItem("color-theme")==="dark"||!("color-theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches){document.documentElement.classList.add("dark")}else{document.documentElement.classList.remove("dark")}tailwind.config={darkMode:"class",theme:{extend:{fontFamily:{sans:["Inter","sans-serif"]}}}};
