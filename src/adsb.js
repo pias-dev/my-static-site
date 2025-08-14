e21d390872eebc2ddc7fb30ff7ac5d07c7c7700f
pias-dev <piaspaul12@gmail.com>
2025-08-14 10:48:33 +0600

    Create adsb.js

---
+++ src/adsb.js
@@ -0,0 +1,12 @@
+function showNativeBannerAd() {
+    document.write(`
+        <script async="async" data-cfasync="false" src="//pl27312178.profitableratecpm.com/849e6610f4501e065f7c0550fff4cc17/invoke.js"><\/script>
+        <div id="container-849e6610f4501e065f7c0550fff4cc17"></div>
+    `);
+}
+
+function showsocialbarAd() {
+    document.write(`
+        <script type='text/javascript' src='//pl27396127.profitableratecpm.com/24/92/2d/24922d458c60e04fa0ccc2c1f9f70062.js'><\/script>
+    `);
+}
