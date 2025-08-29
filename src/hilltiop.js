6d50a7ab6fc0cf97876150d30e6f8f0d8e75b0ba
pias-dev <piaspaul12@gmail.com>
2025-08-29 21:40:18 +0600

    Create hilltiop.js

---
+++ src/hilltiop.js
@@ -0,0 +1,12 @@
+<script>
+(function(thyz){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = thyz || {};
+s.src = "\/\/fancyresponse.com\/b.XOVNsydOGLlb0hYGWQcN\/Heym\/9pu_ZPUJl\/kxP\/TiYU2sMlzCI-2JNtDrIvtXNRjtYlzSMqjmYI0dMbwm";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+</script>
