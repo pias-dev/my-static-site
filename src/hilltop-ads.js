b87db246f7cd4ace118fe0a376c7002ffa6d1f19
pias-dev <piaspaul12@gmail.com>
2025-08-30 21:02:51 +0600

    Update hilltop-ads.js

--- src/hilltop-ads.js
+++ src/hilltop-ads.js
@@ -1,14 +1,3 @@
-(function(tcjx){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = tcjx || {};
-s.src = "//fancyresponse.com/bbX_V.sddRG/l-0wYTWgcL/nezma9JulZzUulnkPPITbY/2CMBzaIG2PN_DMIxt/NjjAYyzkMujgYL0hMSwV";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({})
-
 (function(txbul){
 var d = document,
     s = d.createElement('script'),
