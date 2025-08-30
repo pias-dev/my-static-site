e938bdc2b86076fe122a26fdf71a9cf529fda05e
pias-dev <piaspaul12@gmail.com>
2025-08-30 21:55:59 +0600

    Update video_ad.js

--- src/video_ad.js
+++ src/video_ad.js
@@ -1,10 +1,18 @@
-(function(txbul){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = txbul || {};
-s.src = "\/\/fancyresponse.com\/beX.VcseddGJls0yYgWMcr\/WeKmo9_urZcUKlDkzPATTYw2\/MCzHIb2aMuz\/Q\/tCN\/jYYhzzMhjEYezrNpQd";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({})
+(function(adConfig){
+    var d = document,
+        s = d.createElement("script"),
+        l = d.scripts[d.scripts.length - 1];
+
+    // Apply config if needed
+    s.async = true;
+    s.referrerPolicy = "no-referrer-when-downgrade";
+
+    // Insert ad script URL here (update this if the provider changes)
+    s.src = adConfig.src;
+
+    // Insert before the last loaded script
+    l.parentNode.insertBefore(s, l);
+
+})( {
+    src: "\/\/fancyresponse.com\/beX.VcseddGJls0yYgWMcr\/WeKmo9_urZcUKlDkzPATTYw2\/MCzHIb2aMuz\/Q\/tCN\/jYYhzzMhjEYezrNpQd";
+});
