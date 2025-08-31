f2d7eaf08bf686b530ac7f7eba49e8f901b82f68
pias-dev <piaspaul12@gmail.com>
2025-08-31 10:34:14 +0600

    Delete assets/ads directory

--- assets/ads/in-pagem.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  (s=>{s.dataset.zone=9793871,
-    s.src='https://vemtoutcheeg.com/tag.min.js'})
-    ([document.documentElement, document.body]
-        .filter(Boolean)
-        .pop()
-        .appendChild(document.createElement('script')));
-}, 1000); // 1500ms = 1.5 seconds
