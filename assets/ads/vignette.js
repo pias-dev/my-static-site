1242f5e66d68601e3b66cfc6e266aab036d43997
pias-dev <piaspaul12@gmail.com>
2025-08-31 09:04:45 +0600

    Update vignette.js

--- assets/ads/vignette.js
+++ assets/ads/vignette.js
@@ -1,1 +1,9 @@
-(s=>{s.dataset.zone=9793871,s.src='https://vemtoutcheeg.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+setTimeout(() => {
+  (s => {
+    s.dataset.zone = 9793871;
+    s.src = 'https://vemtoutcheeg.com/tag.min.js';
+  })([document.documentElement, document.body]
+    .filter(Boolean)
+    .pop()
+    .appendChild(document.createElement('script')));
+}, 3000); // 3000ms = 3 seconds
