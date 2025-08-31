e3ff012562630f3ef39f79b7986028f95377d361
pias-dev <piaspaul12@gmail.com>
2025-08-31 08:58:23 +0600

    Update video.js

--- assets/ads/video.js
+++ assets/ads/video.js
@@ -1,11 +1,13 @@
-(function(eku){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = eku || {};
-s.src = "\/\/fancyresponse.com\/bNXaVwsXd.GUln0GYLWzcD\/_eRms9kuoZtUnlSkQPVTPY\/2YMJzwIX2zMoz\/QotoNrjaY\/zCM\/jGYrzqNdQM";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, 2);
-
-})({})
+(function (eku) {
+  setTimeout(function () {
+    var d = document,
+      s = d.createElement("script"),
+      l = d.scripts[d.scripts.length - 1];
+    s.settings = eku || {};
+    s.src =
+      "//fancyresponse.com/bNXaVwsXd.GUln0GYLWzcD/_eRms9kuoZtUnlSkQPVTPY/2YMJzwIX2zMoz/QotoNrjaY/zCM/jGYrzqNdQM";
+    s.async = true;
+    s.referrerPolicy = "no-referrer-when-downgrade";
+    l.parentNode.insertBefore(s, l);
+  }, 2000); // delay in milliseconds (2000 = 2 seconds)
+})({});
