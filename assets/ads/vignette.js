a0614948a74fcdc7e237db058b1595f6159c4aba
pias-dev <piaspaul12@gmail.com>
2025-08-31 10:01:07 +0600

    Delete assets/ads directory

--- assets/ads/vignette.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  (s => {
-    s.dataset.zone = 9793871;
-    s.src = 'https://vemtoutcheeg.com/tag.min.js';
-  })([document.documentElement, document.body]
-    .filter(Boolean)
-    .pop()
-    .appendChild(document.createElement('script')));
-}, 3000); // 3000ms = 3 seconds
