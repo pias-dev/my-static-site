8602f98c45c7bb55d093a84d0dd54498caedd44f
pias-dev <piaspaul12@gmail.com>
2025-08-31 10:34:39 +0600

    Add files via upload

---
+++ assets/ads/in-pagem.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  (s=>{s.dataset.zone=9793871,
+    s.src='https://vemtoutcheeg.com/tag.min.js'})
+    ([document.documentElement, document.body]
+        .filter(Boolean)
+        .pop()
+        .appendChild(document.createElement('script')));
+}, 1000); // 1000ms = 1 seconds
