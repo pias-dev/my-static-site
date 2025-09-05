f5c6e93b332817166bc871b0f47711fcb627c851
pias-dev <piaspaul12@gmail.com>
2025-09-05 15:03:51 +0600

    Delete assets/ads directory

--- assets/ads/interstitial.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  (s=>{s.dataset.zone=9793906,
-    s.src='https://groleegni.net/vignette.min.js'})
-    ([document.documentElement, document.body]
-        .filter(Boolean)
-        .pop()
-        .appendChild(document.createElement('script')))
-}, 3000); // 3000ms = 3 seconds
-
