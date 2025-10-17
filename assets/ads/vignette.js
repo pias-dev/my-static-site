5c8aa3a466f9c783232ba7655ff1021751dac203
pias-dev <piaspaul12@gmail.com>
2025-10-17 23:03:46 +0600

    Delete assets directory

--- assets/ads/vignette.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  (s=>{s.dataset.zone=9793968,
-    s.src='https://gizokraijaw.net/vignette.min.js'})
-    ([document.documentElement, document.body]
-        .filter(Boolean)
-        .pop()
-        .appendChild(document.createElement('script')));
-}, 2000); // 2000ms = 2 seconds
