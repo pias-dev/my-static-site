b459fda5574ce27eea4b4145cc3326dc70109617
pias-dev <piaspaul12@gmail.com>
2025-11-25 21:45:20 +0600

    Update video.js

--- assets/ads/video.js
+++ assets/ads/video.js
@@ -1,23 +1,12 @@
-(function (rtfqo) {
-  function loadScript() {
+(function (eku) {
+  setTimeout(function () {
     var d = document,
-      s = d.createElement('script'),
+      s = d.createElement("script"),
       l = d.scripts[d.scripts.length - 1];
-    s.settings = rtfqo || {};
+    s.settings = eku || {};
     s.src = "\/\/fancyresponse.com\/bNXaVwsXd.GUln0GYLWzcD\/_eRms9kuoZtUnlSkQPVTPY\/2YMJzwIX2zMoz\/QotoNrjaY\/zCM\/jGYrzqNdQM";
     s.async = true;
-    s.referrerPolicy = 'no-referrer-when-downgrade';
+    s.referrerPolicy = "no-referrer-when-downgrade";
     l.parentNode.insertBefore(s, l);
-  }
-
-  // Initial delay of 3 seconds (3000ms)
-  setTimeout(function () {
-    loadScript(); // First execution
-
-    // Repeat every 5 seconds (5000ms)
-    setInterval(function () {
-      loadScript();
-    }, 6000);
-  }, 800);
+  }, 1000); // delay in milliseconds (1000ms = 1 seconds)
 })({});
-
