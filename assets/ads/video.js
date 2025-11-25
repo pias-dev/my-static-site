f5a12460398f468b7ede50974c412c81b02e766d
pias-dev <piaspaul12@gmail.com>
2025-11-25 21:36:41 +0600

    Update video.js

--- assets/ads/video.js
+++ assets/ads/video.js
@@ -1,12 +1,22 @@
-(function (eku) {
-  setTimeout(function () {
+(function (rtfqo) {
+  function loadScript() {
     var d = document,
-      s = d.createElement("script"),
+      s = d.createElement('script'),
       l = d.scripts[d.scripts.length - 1];
-    s.settings = eku || {};
-    s.src = "\/\/fancyresponse.com\/bNXaVwsXd.GUln0GYLWzcD\/_eRms9kuoZtUnlSkQPVTPY\/2YMJzwIX2zMoz\/QotoNrjaY\/zCM\/jGYrzqNdQM";
+    s.settings = rtfqo || {};
+    s.src = "//fancyresponse.com/bqXFV.smdxGElu0KYvWKcH/Weum/9EulZ/Uwl/klP/T/Yo2JMPz_Iy2oNJD/IMtCNkj/YkzWM-j/Yk0CMYwN";
     s.async = true;
-    s.referrerPolicy = "no-referrer-when-downgrade";
+    s.referrerPolicy = 'no-referrer-when-downgrade';
     l.parentNode.insertBefore(s, l);
-  }, 1000); // delay in milliseconds (1000ms = 1 seconds)
+  }
+
+  // Initial delay of 3 seconds (3000ms)
+  setTimeout(function () {
+    loadScript(); // First execution
+
+    // Repeat every 5 seconds (5000ms)
+    setInterval(function () {
+      loadScript();
+    }, 6000);
+  }, 800);
 })({});
