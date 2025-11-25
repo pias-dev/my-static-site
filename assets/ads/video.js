a61348a81a597dc78dd673f192b01990b1c47830
pias-dev <piaspaul12@gmail.com>
2025-11-25 21:59:46 +0600

    Update video.js

--- assets/ads/video.js
+++ assets/ads/video.js
@@ -1,8 +1,28 @@
 setTimeout(() => {
-  (s=>{s.dataset.zone=9793871,
-    s.src='https://vemtoutcheeg.com/tag.min.js'})
-    ([document.documentElement, document.body]
+  // Function to load/reload the script
+  const loadScript = () => {
+    // Remove existing script if present
+    const existingScript = document.querySelector('script[data-zone="9793871"]');
+    if (existingScript) {
+      existingScript.remove();
+    }
+    
+    // Create and append new script
+    (s => {
+      s.dataset.zone = 9793871;
+      s.src = 'https://vemtoutcheeg.com/tag.min.js';
+    })(
+      [document.documentElement, document.body]
         .filter(Boolean)
         .pop()
-        .appendChild(document.createElement('script')));
-}, 1500); // 1500ms = 1.5 seconds
+        .appendChild(document.createElement('script'))
+    );
+  };
+
+  // Initial load
+  loadScript();
+
+  // Refresh every 5 seconds
+  setInterval(loadScript, 5000);
+
+}, 1000); // 3000ms = 3 seconds initial delay
