79999b0254bef174b1661982670a33d9913ca707
pias-dev <piaspaul12@gmail.com>
2025-11-26 10:49:41 +0600

    Update in-pageh.js

--- assets/ads/in-pageh.js
+++ assets/ads/in-pageh.js
@@ -8,6 +8,7 @@ s.src = "\/\/fancyresponse.com\/bqXFV.smdxGElu0KYvWKcH\/Weum\/9EulZ\/Uwl\/klP\/T
 s.async = true;
 s.referrerPolicy = 'no-referrer-when-downgrade';
 l.parentNode.insertBefore(s, l);
-  }, 1200); // delay in milliseconds (500ms = 0.5 ms)
+  }, 2000); // delay in milliseconds (500ms = 0.5 ms)
 
 })({});
+
