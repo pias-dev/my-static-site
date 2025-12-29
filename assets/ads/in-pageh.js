2376481f28a863ad5146cb2a692ae6295c73bc84
pias-dev <piaspaul12@gmail.com>
2025-12-29 17:32:20 +0600

    Add files via upload

--- assets/ads/in-pageh.js
+++ assets/ads/in-pageh.js
@@ -1,22 +1,12 @@
 (function (rtfqo) {
-  function loadScript() {
-    var d = document,
-      s = d.createElement('script'),
-      l = d.scripts[d.scripts.length - 1];
-    s.settings = rtfqo || {};
-    s.src = "\/\/fancyresponse.com\/bqXFV.smdxGElu0KYvWKcH\/Weum\/9EulZ\/Uwl\/klP\/T\/Yo2JMPz_Iy2oNJD\/IMtCNkj\/YkzWM-j\/Yk0CMYwN";
-    s.async = true;
-    s.referrerPolicy = 'no-referrer-when-downgrade';
-    l.parentNode.insertBefore(s, l);
-  }
-
-  // Initial delay of 3 seconds (3000ms)
   setTimeout(function () {
-    loadScript(); // First execution
-
-    // Repeat every 5 seconds (5000ms)
-    setInterval(function () {
-      loadScript();
-    }, 6000);
-  }, 1000);
+    var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = rtfqo || {};
+s.src = "\/\/fancyresponse.com\/bqXFV.smdxGElu0KYvWKcH\/Weum\/9EulZ\/Uwl\/klP\/T\/Yo2JMPz_Iy2oNJD\/IMtCNkj\/YkzWM-j\/Yk0CMYwN";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+  }, 500); // delay in milliseconds (500ms = 0.5 ms)
 })({});
--- assets/ads/in-pagem.js
+++ assets/ads/in-pagem.js
@@ -1,30 +1,8 @@
 setTimeout(() => {
-  // Function to load/reload the script
-  const loadScript = () => {
-    // Remove existing script if present
-    const existingScript = document.querySelector('script[data-zone="9793871"]');
-    if (existingScript) {
-      existingScript.remove();
-    }
-    
-    // Create and append new script
-    (s => {
-      s.dataset.zone = 9793871;
-      s.src = 'https://vemtoutcheeg.com/tag.min.js';
-    })(
-      [document.documentElement, document.body]
+  (s=>{s.dataset.zone=9793871,
+    s.src='https://vemtoutcheeg.com/tag.min.js'})
+    ([document.documentElement, document.body]
         .filter(Boolean)
         .pop()
-        .appendChild(document.createElement('script'))
-    );
-  };
-
-  // Initial load
-  loadScript();
-
-  // Refresh every 5 seconds
-  setInterval(loadScript, 8000);
-
-}, 3000); // 3000ms = 3 seconds initial delay
-
-
+        .appendChild(document.createElement('script')));
+}, 1500); // 1500ms = 1.5 seconds
